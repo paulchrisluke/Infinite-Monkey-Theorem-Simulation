1a282782e63d2c1fe305049850f8cffe8c72a0d8
Paul Chris Luke <paulchrisluke@gmail.com>
2023-06-01 15:49:20 +0800

    highest-score-indicator

--- pages/index.vue
+++ pages/index.vue
@@ -20,6 +20,7 @@
           <div class="mb-4">
             <p>Generated Text:</p>
             <div>{{ generatedText }}</div>
+            <div>Matching Score: {{ calculateMatchingScore() }}%</div>
           </div>
           <div v-if="!targetFound">
             <button class="px-4 py-2 bg-blue-500 text-white rounded" @click="simulateTheorem">Simulate</button>
@@ -31,9 +32,23 @@
         </div>
       </div>
       <div class="lg:w-1/3 mt-8 lg:mt-0">
-        <!-- Right column content -->
-        <img src="https://i.gifer.com/origin/d6/d66620ccdb4aee4182879a2c07d393ef_w200.gif" alt="Monkey Typing" />
-      </div>
+  <!-- Right column content -->
+  <img src="https://i.gifer.com/origin/d6/d66620ccdb4aee4182879a2c07d393ef_w200.gif" alt="Monkey Typing" />
+  <div class="mt-4 p-4 bg-gray-100 rounded-lg">
+    <p class="text-lg font-semibold mb-2">Highest Scoring Attempt:</p>
+    <div class="font-mono" v-if="highestScoringAttempt.generatedText">
+      <span v-for="(char, index) in highestScoringAttempt.generatedText" :key="index" :style="{ background: char === targetText.charAt(index) ? 'lightgreen' : 'transparent' }">
+        {{ char }}
+      </span>
+    </div>
+    <p class="text-sm mt-2">Score: {{ highestScoringAttempt.matchingScore }}%</p>
+    <p class="text-sm mt-1">Attempt Number: {{ highestScoringAttempt.attemptNumber }}</p>
+  </div>
+</div>
+
+
+
+
     </div>
     <footer class="mt-8 py-4 bg-gray-200 text-center">
       <div class="flex items-center justify-center">
@@ -61,18 +76,41 @@ export default {
       simulationRunning: false,
       startTime: null,
       elapsedTime: 0,
-      intervalId: null
+      intervalId: null,
+      highestScoringAttempt: {
+        attemptNumber: 0,
+        generatedText: "",
+        matchingScore: 0,
+      },
     };
   },
   methods: {
     generateRandomText(length) {
-      const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+.,?'\"/:;[]{}|-=<>`~ ";
+      const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+.,?'\"/:;[]{}|-=<>`~ ";
       let randomText = "";
       for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         randomText += characters.charAt(randomIndex);
       }
       return randomText;
+    },
+    calculateMatchingScore() {
+      const targetText = this.targetText.toLowerCase();
+      const generatedText = this.generatedText.toLowerCase();
+      const targetLength = targetText.length;
+      const generatedLength = generatedText.length;
+      let matchingCount = 0;
+
+      for (let i = 0; i < targetLength; i++) {
+        if (generatedText[i] === targetText[i]) {
+          matchingCount++;
+        }
+      }
+
+      const matchingScore = (matchingCount / targetLength) * 100;
+
+      return matchingScore.toFixed(2); // Round to two decimal places
     },
     simulateTheorem() {
       if (!this.simulationRunning) {
@@ -84,10 +122,18 @@ export default {
           this.elapsedTime = Math.floor((currentTime - this.startTime) / 1000);
           this.attempts++;
           this.generatedText = this.generateRandomText(this.targetText.length);
+          const matchingScore = this.calculateMatchingScore();
           if (this.generatedText === this.targetText) {
             this.targetFound = true;
             this.simulationRunning = false;
             clearInterval(this.intervalId);
+          }
+          if (matchingScore > this.highestScoringAttempt.matchingScore) {
+            this.highestScoringAttempt = {
+              attemptNumber: this.attempts,
+              generatedText: this.generatedText,
+              matchingScore: matchingScore,
+            };
           }
         }, 10);
       }
@@ -95,10 +141,10 @@ export default {
     stopSimulation() {
       clearInterval(this.intervalId);
       this.simulationRunning = false;
-    }
+    },
   },
   beforeDestroy() {
     clearInterval(this.intervalId);
-  }
+  },
 };
 </script>
